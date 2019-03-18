b9391e0bd6ea19c01a0346bcd7eaec4833c758e2
Ludvig Hozman <ludvig123@gmail.com>
2019-03-18 22:09:27 +0100

    Add pagination to bdb detail events

--- app/routes/bdb/BdbDetailRoute.js
+++ app/routes/bdb/BdbDetailRoute.js
@@ -21,11 +21,24 @@ import { selectCompanySemesters } from 'app/reducers/companySemesters';
 import { LoginPage } from 'app/components/LoginForm';
 import replaceUnlessLoggedIn from 'app/utils/replaceUnlessLoggedIn';
 import { deleteComment } from 'app/actions/CommentActions';
+import { selectPagination } from 'app/reducers/selectors';
+import createQueryString from 'app/utils/createQueryString';
+
+const queryString = companyId =>
+  createQueryString({
+    company: companyId,
+    ordering: '-start_time'
+  });
 
 const loadData = ({ params: { companyId } }, dispatch) =>
   Promise.all([
     dispatch(fetchSemesters()).then(() => dispatch(fetchAdmin(companyId))),
-    dispatch(fetchEventsForCompany(companyId))
+    dispatch(
+      fetchEventsForCompany({
+        queryString: queryString(companyId),
+        loadNextPage: false
+      })
+    )
   ]);
 
 const mapStateToProps = (state, props) => {
@@ -35,23 +48,40 @@ const mapStateToProps = (state, props) => {
   const companyEvents = selectEventsForCompany(state, { companyId });
   const companySemesters = selectCompanySemesters(state, props);
   const fetching = state.companies.fetching;
+  const showFetchMoreEvents = selectPagination('events', {
+    queryString: queryString(companyId)
+  })(state);
   return {
     company,
     companyId,
     companyEvents,
     comments,
     companySemesters,
-    fetching
+    fetching,
+    showFetchMoreEvents
   };
 };
 
-const mapDispatchToProps = {
-  deleteSemesterStatus,
-  deleteCompanyContact,
-  editSemesterStatus,
-  editCompany,
-  deleteCompany,
-  deleteComment
+const mapDispatchToProps = (dispatch, props) => {
+  const { companyId } = props.params;
+  const fetchMoreEvents = () =>
+    dispatch(
+      fetchEventsForCompany({
+        queryString: queryString(companyId),
+        loadNextPage: true
+      })
+    );
+  return {
+    deleteSemesterStatus: (companyId, semesterStatusId) =>
+      dispatch(deleteSemesterStatus(companyId, semesterStatusId)),
+    deleteCompanyContact: (companyId, constactId) =>
+      dispatch(deleteCompanyContact(companyId, constactId)),
+    editSemesterStatus: data => dispatch(editSemesterStatus(data)),
+    editCompany: data => dispatch(editCompany(data)),
+    deleteCompany: companyId => dispatch(deleteCompany(companyId)),
+    deleteComment: commentId => dispatch(deleteComment(commentId)),
+    fetchMoreEvents
+  };
 };
 
 export default compose(
